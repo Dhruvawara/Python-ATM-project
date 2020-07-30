af29c3227dc7be416cd0b8c05b633f1ae83d724a
Dhruvawara <53032216+Dhruvawara@users.noreply.github.com>
2020-07-30 11:10:49 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -175,10 +175,8 @@ def atm(user_name,Net_balance,Pin,History,acc_no,address):
 
     os.system(clear)
     print ("::: Thanks For Using ATM! :::\n::: We Hope You Are Satisfied With Our Service. :::\n::: Have A Nice Day Ahead. :::")
-    print ("About:")
-    with open('About.txt','r') as infile:
-        show = infile.read()
-        print (show)
+    print ("By : Bindushree Dhruva Harshith")
+
 
     with open(filename,'a+') as ap:
         #rot13() function is called for encoding
